fe6ed90d331e86692a9a2025e6f74e5d66f06d37
Stanislas Bernard <bernardstanislas@gmail.com>
2016-07-05 13:50:18 +0200

    Fix GPIO port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 const gpio = require('gpio');
 const spawn = require('child_process').spawn;
 
-const gpio22 = gpio.export(2, {
+exec('cat *.js bad_file | wc -l', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
+
+const gpio22 = gpio.export(22, {
   direction: 'in',
   ready() {
     console.log('GPIO is ready :)')
@@ -11,6 +20,13 @@ const gpio22 = gpio.export(2, {
 gpio22.on('change', val => {
   if (val === 1) {
     console.log('Will deploy !!');
+    // exec('make -f /home/pi/fastIT/www/dashboard-sirf/current/Makefile staging-fast-deploy', (error, stdout, stderr) => {
+    //   if (error) {
+    //     console.error(`exec error: ${error}`);
+    //   }
+    //   console.log(`stdout: ${stdout}`);
+    //   console.log(`stderr: ${stderr}`);
+    // });
     const deploy = spawn('make', ['-f', '/home/pi/fastIT/www/dashboard-sirf/current/Makefile', 'staging-fast-deploy']);
     deploy.stdout.on('data', data => console.log(`deploy: ${data.toString()}`));
     deploy.stderr.on('data', data => console.log(`deploy [error]: ${data.toString()}`));
