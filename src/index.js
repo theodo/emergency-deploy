2405919f5ccebddca074551603810c0037d9ef8d
nicgirault <nic.girault@gmail.com>
2016-07-05 09:03:25 +0200

    lol

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const gpio = require("gpio");
+// const spawn = require('child_process').spawn;
 const gpio4 = gpio.export(4, {
   direction: "in",
 });
 
 gpio4.on('change', val => {
   console.log(val);
+  // const deploy = spawn('make', ['staging-fast-deploy']);
+  // deploy.stdout.pipe(process.stdout);
 });
