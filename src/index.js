f9574c43f4120c9516eb478c1c3e42d825e6fb11
Stanislas Bernard <bernardstanislas@gmail.com>
2016-07-05 14:43:26 +0200

    Avoid deploy spamming

--- src/index.js
+++ src/index.js
@@ -8,18 +8,17 @@ const gpio22 = gpio.export(22, {
   }
 });
 
+let deploying = false;
+
 gpio22.on('change', val => {
-  if (val === 1) {
+  if (val === 1 && !deploying) {
+    deploying = true;
     console.log('Will deploy !!');
-    // exec('make -f /home/pi/fastIT/www/dashboard-sirf/current/Makefile staging-fast-deploy', (error, stdout, stderr) => {
-    //   if (error) {
-    //     console.error(`exec error: ${error}`);
-    //   }
-    //   console.log(`stdout: ${stdout}`);
-    //   console.log(`stderr: ${stderr}`);
-    // });
     const deploy = spawn('make', ['-f', '/home/pi/fastIT/www/dashboard-sirf/current/Makefile', 'staging-fast-deploy']);
     deploy.stdout.on('data', data => console.log(`deploy: ${data.toString()}`));
     deploy.stderr.on('data', data => console.log(`deploy [error]: ${data.toString()}`));
+    deploy.on('close', () => {
+      deploying = false;
+    });
   }
 });
