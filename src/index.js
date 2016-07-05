a29d83a698d61615d42cc4d10674e5cc68c28d73
Stanislas Bernard <bernardstanislas@gmail.com>
2016-07-05 13:26:56 +0200

    Change GPIO number

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-const gpio = require("gpio");
+const gpio = require('gpio');
 // const spawn = require('child_process').spawn;
-const gpio4 = gpio.export(4, {
-  direction: "in",
+
+const gpio22 = gpio.export(2, {
+  direction: 'in',
+  ready() {
+    console.log('GPIO is ready :)')
+  }
 });
 
-gpio4.on('change', val => {
+gpio22.on('change', val => {
   console.log(val);
   // const deploy = spawn('make', ['staging-fast-deploy']);
   // deploy.stdout.pipe(process.stdout);
