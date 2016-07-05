d847cdcc3b17117748ae3ed0694aa2459296967f
Stanislas Bernard <bernardstanislas@gmail.com>
2016-07-05 13:47:54 +0200

    Add missing condition

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const gpio = require('gpio');
-// const spawn = require('child_process').spawn;
+const spawn = require('child_process').spawn;
 
 const gpio22 = gpio.export(2, {
   direction: 'in',
@@ -9,7 +9,10 @@ const gpio22 = gpio.export(2, {
 });
 
 gpio22.on('change', val => {
-  const deploy = spawn('make', ['-f', '/home/pi/fastIT/www/dashboard-sirf/current/Makefile', 'staging-fast-deploy']);
-  deploy.stdout.on('data', data => console.log(`deploy: ${data.toString()}`));
-  deploy.stderr.on('data', data => console.log(`deploy [error]: ${data.toString()}`));
+  if (val === 1) {
+    console.log('Will deploy !!');
+    const deploy = spawn('make', ['-f', '/home/pi/fastIT/www/dashboard-sirf/current/Makefile', 'staging-fast-deploy']);
+    deploy.stdout.on('data', data => console.log(`deploy: ${data.toString()}`));
+    deploy.stderr.on('data', data => console.log(`deploy [error]: ${data.toString()}`));
+  }
 });
